369ebf8546071f3e9485b6a4cddc0d028865bbd6
fedorloopanov <zoranyovovich@gmail.com>
2026-04-18 12:58:05 +0300

    Update MissionParserRegistry.java

--- MissionParserRegistry.java
+++ MissionParserRegistry.java
@@ -1,22 +1,32 @@
-package app.model;
+package app.parser;
 
-public class Sorcerer {
-    private String name;
-    private String rank;
+import app.exception.MissionParseException;
 
-    public String getName() {
-        return name;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MissionParserRegistry {
+    private final List<MissionParser> parsers = new ArrayList<>();
+
+    public MissionParserRegistry register(MissionParser parser) {
+        parsers.add(parser);
+        return this;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public MissionParser getParser(File file) throws MissionParseException {
+        for (MissionParser parser : parsers) {
+            if (parser.supports(file)) {
+                return parser;
+            }
+        }
+        throw new MissionParseException("Формат файла не поддерживается: " + file.getName());
     }
 
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
+    public String supportedFormats() {
+        return parsers.stream()
+                .map(MissionParser::getFormatName)
+                .collect(Collectors.joining(", "));
     }
 }
